6318b83fe520a9da5bc0446a7fbd491e82789e17
Frank Thomsen Renard <iamfrank@users.noreply.github.com>
2017-06-22 12:49:28 +0200

    Add files via upload

---
+++ template-parts/footer/site-info.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Cordiq
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info footer">
+	
+    <div>
+        <span>CORDIQ</span>
+        <span>Flaesketorvet 28</span>
+        <span>1711 Copenhagen</span>
+        <span>CVR: 32143504</span>
+        <span>+45 61619595</span>
+        <span>
+            <script>
+                document.write('<'+'a'+' '+'h'+'r'+'e'+'f'+'='+"'"+'m'+'a'+'i'+'l'+'t'+'o'+'&'+'#'+'5'+'8'+';'+'&'+'#'+'3'+'7'+';'+
+                    '6'+'6'+'&'+'#'+'9'+'8'+';'+'&'+'#'+'6'+'4'+';'+'%'+'6'+'3'+'&'+'#'+'1'+'1'+'1'+';'+'r'+'d'+'&'+'#'+
+                    '1'+'0'+'5'+';'+'q'+'&'+'#'+'4'+'6'+';'+'c'+'o'+'m'+"'"+'>'+'f'+'&'+'#'+'9'+'8'+';'+'&'+'#'+'6'+'4'+
+                    ';'+'c'+'o'+'&'+'#'+'1'+'1'+'4'+';'+'d'+'i'+'&'+'#'+'1'+'1'+'3'+';'+'&'+'#'+'4'+'6'+';'+'c'+'&'+'#'+
+                    '1'+'1'+'1'+';'+'m'+'<'+'/'+'a'+'>');
+            </script>
+            <noscript>
+                [Turn on JavaScript to see the email address]
+            </noscript>
+        </span>
+        <span>
+            <a href="https://twitter.com/cordiq_">
+                Twitter
+                <img style="width: .8rem;" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/twitter.svg" alt="Twitter">
+            </a>
+            <a href="https://www.linkedin.com/company/17906399?trk=tyah&amp;trkInfo=clickedVertical%3Acompany%2CclickedEntityId%3A17906399%2Cidx%3A2-1-2%2CtarId%3A1486373282870%2Ctas%3Acordiq">
+                LinkedIn
+                <img style="width: .8rem;" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/linkedin.svg" alt="LinkedIn">
+            </a>
+        </span>
+        <span>Copyright Cordiq © 2017</span>
+    </div>
+    
+</div><!-- .site-info -->
